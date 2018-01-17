dd73f4dde01d923ee4e7be80827969faa1d739c1
scottbarclay <scottdbarclay@gmail.com>
2018-01-16 17:01:44 -0800

    add solutions to first, last, each, filter, indexOf, Uniq

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,16 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    // return n === undefined ? array[array.length - 1] : Math.max(array.slice(0, n));
+    if(arguments.length === 1 ) {
+      return array[array.length - 1];
+    } else if(n === 0) {
+      return [];
+    } else if(n > array.length - 1) {
+      return array;
+    } else {
+      return array.slice(array.length - n, array.length);
+    } 
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +56,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection)){
+      for(var i = 0; i < collection.length; i++){
+        iterator(collection[i], i, collection);
+      }
+    } else{
+        for (var key in collection){
+          iterator(collection[key], key, collection);
+        }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,6 +86,26 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection, function(item) {
+      if(test(item)) {
+        result.push(item);
+      }
+    });
+
+    // _.each(collection, test);
+
+
+    // _.each(collection, test(key) {
+       
+
+    //   if(test(key)) {
+    //     result.push(collection[key]);
+    //   }
+    // });
+
+
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
